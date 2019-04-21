0436cc63879916ce399ee32b9bd81254214eb0b9
Damian Rgues <damian.rodriguess@gmail.com>
2019-04-21 14:41:25 +0200

    fix music game over error

--- js/game.js
+++ js/game.js
@@ -13,7 +13,7 @@ function Game (canvas) {
   this.gameSound = new Audio ("../music/game-music.mp3");
   this.collisionSound = new Audio ("../music/boton-try-retry.mp3");
   this.winSound = new Audio ("../music/index-music.mp3");
-  this.gameOverSound = new Audio ("../music/gameover.mp3");
+  this.gameOverSound = new Audio ("../music/gameover_good.mp3");
  
   
   
